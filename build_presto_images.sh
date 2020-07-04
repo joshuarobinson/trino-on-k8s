1c91f0b97c3b7b58bdcdec5ad6697b42e50d74c7
Joshua Robinson <joshua@purestorage.com>
2020-07-04 14:32:34 -0700

    Update to newest version (337), tweak configs.
    Update image and improve some configuration settings for larger
    clusters.

--- build_presto_images.sh
+++ build_presto_images.sh
@@ -3,13 +3,27 @@
 set -e
 
 REPONAME=joshuarobinson
-PRESTOVER=330
+PRESTOVER=337
 
-docker build --build-arg PRESTO_VER=$PRESTOVER -t fb-presto .
+## Build Presto-server
+
+docker build --build-arg PRESTO_VER=$PRESTOVER -t fb-presto presto/
 
 # Tag and push to the public docker repository.
 docker tag fb-presto $REPONAME/fb-presto
 docker push $REPONAME/fb-presto
 
 # Update configMap in Kubernetes
+pushd presto/
 kubectl create configmap presto-cfg --dry-run --from-file=config.properties.coordinator --from-file=config.properties.worker --from-file=node.properties.template --from-file=hive.properties.template -o yaml | kubectl apply -f -
+popd
+
+## Build Presto-CLI
+
+TAG=presto-cli
+
+docker build --build-arg PRESTO_VER=$PRESTOVER -t $TAG presto-cli/
+
+# Tag and push to the public docker repository.
+docker tag $TAG $REPONAME/$TAG
+docker push $REPONAME/$TAG
